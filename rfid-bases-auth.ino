5f96c44ab0c1bc008eb664a6297679ad9c649ad5
dmigwi <migwindungu0@gmail.com>
2024-03-31 19:12:11 +0200

    Initial commit

---
+++ rfid-bases-auth.ino
@@ -0,0 +1,28 @@
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// LCD Pin configuration
+#define LCD_RST 
+#define LCD_RW 
+#define LCD_EN 
+#define LCD_D0
+#define LCD_D1
+#define LCD_D2
+#define LCD_D3
+#define LCD_D4 
+#define LCD_D5 
+#define LCD_D6 
+#define LCD_D7 
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
