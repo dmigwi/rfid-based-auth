4593474b9e95672abbfcdcc48eb0e5d20c9ac221
Migwi Ndung'u <22055953+dmigwi@users.noreply.github.com>
2024-08-07 15:17:20 +0200

    Setup Reproducible Builds (#3)
    * Add arduino cli configuration files
    
    * Implement reproduceable builds using sketch.yaml file
    
    * Add BSD License
    
    * Add a makefile to simplify the build and compiling process

---
+++ rfid-plus-display/picc.cpp
@@ -0,0 +1,18 @@
+/*!
+ * @file picc.cpp
+ *
+ * @section intro_sec Introduction
+ *
+ * This file is part rfid-plus-display package files. It is an implementation
+ * of the RFID PICC that runs on a adapter customised for AVR boards.
+ * It may work with other AVR boards but that cannot be guaranteed.
+ *
+ * @section author Author
+ *
+ * Written by dmigwi (Migwi Ndung'u)  @2024
+ * LinkedIn: https://www.linkedin.com/in/migwi-ndungu/
+ *
+ *  @section license License
+ *
+ * BSD license, all text here must be included in any redistribution.
+ */
