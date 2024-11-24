cd62b75db4ea4cb77698c15b7c47834cb940da7a
dmigwi <migwindungu0@gmail.com>
2024-11-24 19:56:07 +0100

    Add rfid interrupt handler function

--- rfid-plus-display/rfid-plus-display.ino
+++ rfid-plus-display/rfid-plus-display.ino
@@ -19,9 +19,9 @@
 
 #include "Arduino.h"
 
-#include <LiquidCrystal.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <LiquidCrystal.h>
 
 namespace Settings
 {
@@ -32,6 +32,10 @@ namespace Settings
     // REFRESH_DELAY defines how long it takes to refresh the display when
     // displaying a scrolling message. 
     inline constexpr int REFRESH_DELAY {700};
+
+    // Transmitter defines in this namespace because the interrupt handler method
+    // cannot be a class method. 
+    Transmitter rfid;
 };
 
 // Display manages the relaying the status of the internal workings to the
@@ -51,6 +55,34 @@ class Display
             m_lcd.begin(displayCols, displayRows);
         }
 
+        // setMainMessage sets the main message that is to be displayed on Row 1.
+        // This message is mostly concise with clear message and doesn't require
+        // scrolling.
+        void setMainMessage(const String& data)
+        {
+            m_message = data;
+        }
+
+        // setStatusMessage set the status message that is to be displayed on Row 2.
+        // This message is usually a longer explanation of the main message and
+        // may be scrollable.
+        void setStatusMessage(const String& data)
+        {
+            m_status = data;
+        }
+
+        // print refreshes the display so that messages longer than max characters
+        // supported can be scrolled from right to left.
+        void print()
+        {
+            if (!m_message.length() > 0)
+                print(m_message, 0, 0); // print on Row 1
+
+            if (!m_status.length() > 0)
+                print(m_status, 0, 1); // print on Row 2
+        }
+    
+    protected:
         // print outputs the content on the display. It also manages scrolling 
         // if the character count is greater than the LCD can display at a go.
         void print(const String& data, int col = 0, int line = 0)
@@ -66,13 +98,15 @@ class Display
                 // Print a message to the LCD.
                 m_lcd.print(data.substring(startIndex++, endIndex++));
 
-                // delay for a 500ms
+                // delay
                 delay(Settings::REFRESH_DELAY);
             } while (endIndex <= charCount);
         }
 
     private:
         LiquidCrystal m_lcd;
+        String m_message {""};
+        String m_status{""};
 
         // displayCols defines the number of columns that the LCD supports.
         const int displayCols {16};
@@ -102,37 +136,63 @@ class Transmitter
         }
 
         // readPICC reads the contents of a given Proximity Inductive Coupling Card (PICC/NFC Card)
-        char* readPICC() {}
+        char* readPICC() 
+        {
+            // Stage 1: Activate the Card (Request + Anticollision + Select)
+            //  - Card is activated and UID is retrieved. Card is ready for next operations.
+            // Stage 2: Authentication
+            // - Three pass authentication sequence handled by the reader automatically
+            // Stage 3: Read the Card block/sector contents.
+        }
 
         void writePICC(const char* data) {}
 
     private:
         MFRC522 m_rc522;
-
 };
+
+// handleDetectedCard on detecting an NFC card within the field, an interrupt
+// is triggered which forces reading and writting of the necessary data to
+// the card to be done as a matter of urgency.
+void handleDetectedCard()
+{
+    if (Settings::rfid.isNewCardDetected())
+    {
+        // TODO: Set the Card Reading status to the display.
+        const char* cardData {Settings::rfid.readPICC()};
+
+        // TODO: Indicate success or failure of the card reading operation.
+
+        // TODO: Set the card's serial writting status to the WIFI module.
+        Serial.print(cardData);
+
+        // TODO: on feedback recieved, set the receiving message status.
+
+        // TODO: set the card writting status.
+        Settings::rfid.writePICC(cardData);
+    }
+}
 
 void runSerialPassthrough()
 {
-    if (serialEventRun) serialEventRun();
-
+    #ifdef ARDUINO_AVR_LEONARDO
     // SerialPassthrough code is only run when needed.
     // https://docs.arduino.cc/built-in-examples/communication/SerialPassthrough/
-    #ifdef ARDUINO_AVR_LEONARDO
     // A serial passthrough is necessary because leonardo board doesn't have
     // a chip dedicated to managing the serial communication via UART protocol.
     // Copy from virtual serial line to uart and vice versa
     if (Serial.available()) {
-        while(Serial.available()){
+        while(Serial.available())
             Serial1.write(Serial.read());
-        }
     }
 
     if (Serial1.available()) {
-        while(Serial1.available()) {
+        while(Serial1.available())
             Serial.write(Serial1.read());
-        }
     }
     #endif
+
+    if (serialEventRun) serialEventRun();
 }
 
 int main(void)
@@ -152,25 +212,13 @@ int main(void)
     const int LCD_D6 {7};
     const int LCD_D7 {6};
 
-    /*
-    * Typical pin layout used:
-    * -----------------------------------------------------------------------------------------
-    *             MFRC522      Arduino       Arduino   Arduino    Arduino Arduino
-    *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro
-    * Pro Micro Signal          Pin          Pin           Pin       Pin        Pin Pin
-    * -----------------------------------------------------------------------------------------
-    * RST/Reset    RST           9         5          D9            RESET/ICSP-5 RST
-    * SPI SS       SDA(SS)       10        53         D10           10 10 SPI MOSI
-    * MOSI         11 / ICSP-4   51        D11        ICSP-4        16 SPI MISO
-    * MISO         12 / ICSP-1   50        D12        ICSP-1        14 SPI SCK
-    * SCK          13 / ICSP-3   52        D13        ICSP-3        15
-    *
-    * SS and RST pins are configurable.
-    */
-
     // PCD Transmitter Pins 
     const int RFID_RST {22}; // A4 Pin
     const int RFID_SS {23};  // A5 Pin
+    const int RFID_IRQ {2}; // Interrupt pin
+
+    // setup the IRQ pin
+    pinMode(RFID_IRQ, INPUT_PULLUP);
 
     Serial.begin(Settings::SERIAL_BAUD_RATE);
     Serial1.begin(Settings::SERIAL_BAUD_RATE);
@@ -180,29 +228,20 @@ int main(void)
         LCD_D4, LCD_D5, LCD_D6, LCD_D7 // 4-bit UART data pins
     };
 
-    lcdDisplay.print("Hello, Warszawa!", 0, 0);
+    lcdDisplay.setMainMessage("Hello, Warszawa!");
+    lcdDisplay.setStatusMessage("The weather today is quite cold for me!");
 
-    lcdDisplay.print("The weather today is quite cold for me!", 1, 1);
+    Settings::rfid = Transmitter {RFID_SS, RFID_RST};
 
-    Transmitter rc522 {RFID_SS, RFID_RST};
+    // Activate the RFID interrupt on the RISING mode.
+    attachInterrupt(digitalPinToInterrupt(RFID_IRQ), handleDetectedCard, RISING);
     
 	while(true) {
-        if (rc522.isNewCardDetected())
-        {
-            // TODO: Set the Card Reading status to the display.
-            const char* cardData {rc522.readPICC()};
-
-            // TODO: Indicate success or failure of the card reading operation.
-
-            // TODO: Set the card's serial writting status to the WIFI module.
-            Serial.print(cardData);
-
-            // TODO: on feedback recieved, set the receiving message status.
-
-            // TODO: set the card writting status.
-            rc522.writePICC(cardData);
-        }
+        // refresh the display.
+        lcdDisplay.print();
+       
         runSerialPassthrough();
+        delay(Settings::REFRESH_DELAY);
 	}
         
 	return 0;
