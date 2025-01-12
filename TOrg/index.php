5e2cb8580b9ebc238bbe04bf1d1be68929c581c5
dmigwi <migwindungu0@gmail.com>
2025-01-12 18:09:19 +0100

    Update the API endpoints for the torg

--- TOrg/index.php
+++ TOrg/index.php
@@ -8,19 +8,18 @@
 	$deviceExists = false;
 
 	function md5hash($data) {
-            return md5(strtoupper($data));
+        return hash("md5", strtoupper($data));
     }
 
 	function sha256hash($data) {
-            return hash("sha256", strtoupper($data));
+        return hash("sha256", strtoupper($data));
     }
-
 
 	function insertTrustKey ($new_trustkey, $deviceUid, $secretKeyId) {
         try {
             global $con;
             global $trustOrgId;
-           
+
             $new_block2data = md5hash($trustOrgId . $deviceUid);
             //echo " * new block2data :".$new_block2data. "\n";
 
@@ -33,70 +32,68 @@
                 return $new_trustkey . $trustOrgId . $deviceUid;
             }
         } catch (Exception $e) {
-                //echo $e;
+            //echo $e;
         }
         return "";
     };
 
 	function findDevice($device_uid) {
-         try {
+        try {
             global $con;
             global $inTrustOrgMode;
             global $deviceExists;
- 
+
             $query = "SELECT is_trust_org FROM `devicesTable` WHERE device_id='$device_uid'";
             $result = mysqli_query($con, $query);
 
             $deviceExists = ($result && mysqli_num_rows($result) > 0);
-            
+
             if ($deviceExists) {
                 $inTrustOrgMode = (mysqli_fetch_row($result)[0] == 1);
             }
-                 
+
             mysqli_free_result($result);
-          } catch (Exception $e) {
-                 //echo $e;
-         }
+        } catch (Exception $e) {
+            //echo $e;
+        }
     }
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Handle POST request.
-        
+
         $data = file_get_contents('php://input');
         //$data = hex2bin($data); // For postman testing only.
-            
         //echo "Raw data :" . bin2hex($data). "\n";
-        
+
         $hashed_tag_uid = "";
         $device_uid = "";
         $default_trustkey = sha256hash("Who let the dogs out!");
-            
+
         // Data packing
         if (strlen($data) >= 19) {
             $uid_size = hexdec(bin2hex(substr($data, 0, 1)));
             $hashed_tag_uid = md5hash(bin2hex(substr($data, 1, $uid_size)));
             $device_uid = bin2hex(substr($data, 11, 8));
         }
-            
+
         try {
             findDevice($device_uid);
-                
-             //echo "content length: " . $_SERVER["CONTENT_LENGTH"]. "\n";
-             //echo "string length: " . strlen($data). "\n";
+            //echo "content length: " . $_SERVER["CONTENT_LENGTH"]. "\n";
+            //echo "string length: " . strlen($data). "\n";
 
             // Content length for secret key authentication is 35 (in Ascii format)
-            if ($_SERVER["CONTENT_LENGTH"] >= 35 && strlen($data) == 35) {  
-        
+            if ($_SERVER["CONTENT_LENGTH"] >= 35 && strlen($data) == 35) {
+
                 $query = "SELECT secret_key FROM `secretKeysTable` WHERE hashed_tag_uid='$hashed_tag_uid'";
                 $result = mysqli_query($con,$query);
-                
+
                 if (mysqli_num_rows($result) > 0) {
                     $response = mysqli_fetch_row($result)[0];
                 }
                 mysqli_free_result($result);
                 //echo " Secret Key: ".$response. " \n";
-                
-                
+
+
                 if (empty($response) && $inTrustOrgMode) { // new card update by trust organization pcd.
                     $secret_key = substr(md5hash(random_bytes(8).$hashed_tag_uid), 0, 12);
                     $query = "INSERT INTO `secretKeysTable` (hashed_tag_uid, secret_key) VALUES('%s', '%s')";
@@ -111,9 +108,9 @@
                     // Also insert default trust key entry.
                     insertTrustKey($default_trustkey, $device_uid, $secret_key_id);
                 }
-            
+
                 if (!empty($response)) { // Previous entry exists, validate block 2 data now.
-                            
+
                     $hashed_block2data = md5hash(bin2hex(substr($data, 19, 16)));
                     //echo " -block2data :".$hashed_block2data. "\n";
 
@@ -121,20 +118,20 @@
                             "ORDER BY created_on DESC LIMIT 1";
                     $query = sprintf($query, $hashed_block2data);
                     $result = mysqli_query($con, $query);
-                    
+
                     //echo " Query: ".$query. " \n";
-                    
+
                     if (!$result || mysqli_num_rows($result) != 1){
                         $response = ""; // Unexpected error occured
                     }
-                
+
                     mysqli_free_result($result);
                 } else {
                     // An error occured and the secret key couldn't be retrieved or it doesn't exist.
-                    $response = "Malformed request body found!-04";
+                    $response = bin2hex("Malformed request body found!-04");
                 }
             }
-            
+
             if ($_SERVER["CONTENT_LENGTH"] >= 67 && strlen($data) == 67) {
                 // Content length for trust key authentication is 67
 
@@ -143,7 +140,7 @@
                 $old_trustkey = bin2hex(substr($data, 19, 32));
                 $old_block2data  = md5hash(bin2hex(substr($data, 51, 16)));
                 //echo " --block2data :".$old_block2data. "\n";
-                
+
                 // picks only the most recent trust key insert for authentication.
                 $query = "SELECT t.s FROM (".
                 			"SELECT `secret_key_id` as s, `rolling_pass` as r  from `rollingPasswordTable` as m ".
@@ -152,17 +149,16 @@
                 $query = sprintf($query, $old_block2data, $default_trustkey, $old_trustkey);
                 $result = mysqli_query($con, $query);
                 //echo $query . " \n";
-                
+
                 $secret_key_id = -1;
                 if ($result && mysqli_num_rows($result) > 0) {
                     $secret_key_id = mysqli_fetch_row($result)[0];
                     $prevTrustKeyMatches = true;
                 }
-                
+
                 mysqli_free_result($result);
 
                 // Validate the device uid provided if it is among the supported.
-
                 if($deviceExists && $secret_key_id != -1) {
                     if ($prevTrustKeyMatches || $inTrustOrgMode) {
                         $new_trustkey = sha256hash(random_bytes(8).$old_trustkey);
@@ -172,60 +168,55 @@
 
                 if (empty($response)){
                     // An error occured and the secret key couldn't be retrieved or it doesn't exist.
-                    $response = "Malformed request body found!-05";
+                    $response = bin2hex("Malformed request body found!-05");
                 }
             }
         } catch (Exception $e) {
             //echo $e;
         }
 
-            //echo $response; //For postman testing only.
-            echo hex2bin(strtoupper($response));
+        //echo $response; //For postman testing only.
+        echo hex2bin($response);
     } else {
         // Handle GET request.
-        $query = "SELECT hashed_blockdata, rolling_pass, created_on FROM rollingPasswordTable ORDER BY created_on LIMIT 10";
-        $result = mysqli_query($con, $query);
-        
-        echo "<style>
-                table {
-                  font-family: arial, sans-serif;
-                  border-collapse: collapse;
-                  width: 100%;
-                }
-
-                td, th {
-                  border: 1px solid #dddddd;
-                  text-align: left;
-                  padding: 8px;
-                }
-
-                tr:nth-child(even) {
-                  background-color: #dddddd;
-                }
-			</style>";
-        echo "<h1>10 Most Recent Successful RFID Authentications. </h1>";
-         echo "<table>
-            <tr>
-                <th>Created On</th>
-                <th>Hashed BlockData</th>
-                <th>Rolling Password</th>
-            </tr>";
- 
-        if ($result && mysqli_num_rows($result) > 0) {
-            while($row = mysqli_fetch_assoc($result)) {
-                $datetime = new DateTime($row["created_on"]); 
-                echo "<tr><td>".$datetime->format(DateTimeInterface::RFC2822)."</td>".
-                        "<td>".$row["hashed_blockdata"]. "</td>".
-                        "<td>".$row["rolling_pass"]."</td>".
-                    "</tr>";
-            }
+        $page = 1;
+        if (!empty($_GET) && !empty($_GET["page"])) {
+            $page = max(1, intval($_GET["page"]));
         }
 
-        echo "</table>";
-        mysqli_free_result($result);
+        $offset = ($page-1) * 10;
+
+        try {
+            $query = "SELECT hashed_blockdata, rolling_pass, created_on FROM rollingPasswordTable ORDER BY created_on DESC LIMIT 10 OFFSET ".$offset;
+            $result = mysqli_query($con, $query);
+
+            $data = array();
+
+            if ($result && mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                     array_push($data, (object)[
+                        "created_on" => $row["created_on"],
+                        "hashed_block2data" => $row["hashed_blockdata"],
+                        "rolling_password" => substr($row["rolling_pass"],0,10)."...".substr($row["rolling_pass"],-10),
+                    ]);
+                }
+            }
+
+            mysqli_free_result($result);
+        } catch (Exception $e) {
+            //echo $e;
+        }
+
+        $info = (object)[
+              "currentpage" => $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"].$_SERVER["PHP_SELF"]."?page=".$page,
+              "data" => $data,
+        ];
+
+        header("Content-Type:application/json");
+        echo json_encode($info);
     }
 
-    // echo $data;
+    //echo $data;
     //echo json_encode($_SERVER);
     mysqli_close($con);
 ?>
